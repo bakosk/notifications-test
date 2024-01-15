137e683d38eecad0158b1870ec3729fe8c6cac44
bako <martiniak.matej@gmail.com>
2024-01-15 21:55:35 +0100

    added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'notifications-test';
+
+  private readonly VAPID_PUBLIC_KEY = "BFD5OmqY4gF1wvBs3KVylyE_XnG4lTywOH9sCcqeDFj2yLB1AOmzV9hGbm0FKdsCjjrFgcg4u0-hDrJW_kuHbM0";
+  private readonly swPush = inject(SwPush);
+
+    subscribeToNotifications() {
+        this.swPush.requestSubscription({
+            serverPublicKey: this.VAPID_PUBLIC_KEY
+        })
+        .then(sub => console.log(sub))
+        .catch(err => console.error("Could not subscribe to notifications", err));
+    }
 }
